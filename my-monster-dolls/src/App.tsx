fbed08bcdc11b0a2769ed2d71f0809cf2b82a444
Olga <tomieForever@gmail.com>
2024-05-06 20:10:38 +0200

    rewrite scss, add components, add template

--- my-monster-dolls/src/App.tsx
+++ my-monster-dolls/src/App.tsx
@@ -1,15 +1,38 @@
 //import { useState } from 'react'
-import './App.scss'
+import './scss/app.scss'
+import Categories from './components/Categories'
+import Sort from './components/Sort'
+import DollBlock from './components/DollBlock';
 import Header from './components/Header'
 
 function App() {
  // const [count, setCount] = useState(0)
 
   return (
-    <div className='wrapper clear'>
-    <Header></Header>
-      <h1 >Hello world!</h1>
+    <div className="App">
+    <div className="wrapper">
+      <Header></Header>
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories></Categories>
+            <Sort></Sort>
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <DollBlock title='Laguna Blue' price={500}></DollBlock>
+            <DollBlock></DollBlock>
+            <DollBlock></DollBlock>
+            <DollBlock></DollBlock>
+            <DollBlock></DollBlock>
+
+          </div>
+        </div>
+      </div>
     </div>
+
+
+  </div>
   )
 }
 
