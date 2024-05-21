3c3193354e7cf58a624254ca272bbd3637565d6f
Olga <tomieForever@gmail.com>
2024-05-21 17:45:47 +0200

    add pagination

---
+++ my-monster-doll-js/src/components/Pagination/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import s from './index.module.scss'
+import ReactPaginate from 'react-paginate'
+
+export default function Pagination({count, onChangePage, currentPage}) {
+    
+  return (
+    <div className={s.root}>
+      <ReactPaginate
+     className={s.container}
+         breakLabel='...'
+         nextLabel='>'
+         onPageChange={(event)=> onChangePage (event.selected+1)}
+         pageRangeDisplayed={3}
+         pageCount={Math.ceil(count/6)}
+         forcePage ={currentPage-1}
+         previousLabel='<'
+         renderOnZeroPageCount={null}
+         />
+    </div>
+  )
+}
--- my-monster-doll-js/src/pages/Home.jsx
+++ my-monster-doll-js/src/pages/Home.jsx
@@ -6,16 +6,23 @@ import Sort from '../components/Sort'
 import DollsBlock from '../components/DollsBlock'
 import Skeleton from '../components/DollsBlock/Skeleton'
 import { SearchContext } from '../App'
+import Pagination from '../components/Pagination'
 export default function Home() {
  const [dolls, setDolls] = React.useState([])
  const [isLoading, setIsLoading] = React.useState(true)
- const { searchValue } = React.useContext(SearchContext);
+
+ const { searchValue } = React.useContext(SearchContext)
+ 
 
 // ////////////////////////////////////////////////////
  const [sort, setSort] = React.useState({
     name: 'age (DESC)', sortProperty: 'year'
  })
  const[activeCategory, setActiveCategory] = React.useState('All')
+ const [currentPage, setCurrentPage] =React.useState(1)
+ const [countItems, setCountItems]= React.useState(1)
+///////////////////////////////////////////////////////
+
   React.useEffect (()=>{
     setIsLoading(true)
 
@@ -24,13 +31,14 @@ export default function Home() {
    const order = sort.sortProperty.includes ('-') ? 'asc' : 'desc'
    const search = searchValue ? `&q=${searchValue}` : '';
    
-    axios.get(`http://localhost:3000/dolls?${category}&_sort=${sortBy}&_order=${order}${search}`).
+    axios.get(`http://localhost:3000/dolls?_page=${currentPage}&_limit=6&${category}&_sort=${sortBy}&_order=${order}${search}`).
     then((res)=>{
       setDolls(res.data)
+      setCountItems(res.headers["x-total-count"])
       setIsLoading(false)
     })
     window.scrollTo(0,0)
-  },[activeCategory, searchValue, sort])
+  },[activeCategory, currentPage, searchValue, sort])
   
   const dataDolls = dolls.map((obj)=> 
     <DollsBlock key={obj.id} {...obj}></DollsBlock>)
@@ -46,6 +54,7 @@ export default function Home() {
           <div className="content__items">
           {isLoading ? [...new Array(6)].map((_, index)=><Skeleton key={index}></Skeleton>) : dataDolls}
           </div>
+          <Pagination count={countItems} onChangePage={(number)=>setCurrentPage(number)} currentPage={currentPage}></Pagination>
         </div>
     </>
   )
