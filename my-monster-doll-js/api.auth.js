5ece5a43cfad42b5b247dab04a63dd944972ab25
Olga <tomieForever@gmail.com>
2024-06-17 14:10:53 +0200

    add auth

---
+++ my-monster-doll-js/api.auth.js
@@ -0,0 +1,15 @@
+import { instance } from "./api.config.js";
+export default const AuthService {
+
+    login (email, password) {
+        return instance.post("/api/login", { email, password })
+    }
+
+refreshToken() {
+    return instance.get("/api/refresh");
+}
+
+logout() {
+    return instance.post("/api/logout")
+}
+}
